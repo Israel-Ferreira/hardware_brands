9be933c1d3d4b499b1e5ba3a7e0e1470ab1391a1
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-02-13 01:57:21 -0300

    Fazendo a tela de marcas

--- app/controllers/hardware_brands_controller.rb
+++ app/controllers/hardware_brands_controller.rb
@@ -1,2 +1,17 @@
 class HardwareBrandsController < ApplicationController
+
+  before_action :set_hardware_brand, only: [:show]
+
+  def index
+    @hardware_brands = HardwareBrand.all 
+  end
+
+  def show
+  end
+
+  private
+
+  def set_hardware_brand
+    @hardware_brand = HardwareBrand.find(params[:id])
+  end
 end
